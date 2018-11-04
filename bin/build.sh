47a14ef652ced4f931fe29591eaa88b5e1f5f366
xiaogang <715709084@qq.com>
2018-11-04 19:56:25 +0800

    update shell origin

--- bin/build.sh
+++ bin/build.sh
@@ -1,9 +1,9 @@
 #!bin/sh
 
-docker login --username=txg5214 registry.cn-shenzhen.aliyuncs.com
+docker login --username=txg5214 registry-vpc.cn-shenzhen.aliyuncs.com
 
 docker build -t vinda-video .
 
-docker tag vinda-video:latest registry.cn-shenzhen.aliyuncs.com/vinda/vinda-video:1.0.0
+docker tag vinda-video:latest registry-vpc.cn-shenzhen.aliyuncs.com/vinda/vinda-video:1.0.0
 
-docker push registry.cn-shenzhen.aliyuncs.com/vinda/vinda-video:1.0.0
+docker push registry-vpc.cn-shenzhen.aliyuncs.com/vinda/vinda-video:1.0.0
